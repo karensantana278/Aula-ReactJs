7f30b593f48101d4ac5a1e66e472e857981e54af
Myllena Lucena <karensantana278@gmail.com>
2021-11-03 22:29:34 -0300

    Instalação Reactjs

---
+++ src/views/Home/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import * as S from './styles';
+
+function Home() {
+    return (
+        <S.Container>
+            <h2> Olá Mundo </h2>
+            {/* Header */}
+
+            <S.FilterArea>
+            {/* FilterCard */}
+            </S.FilterArea>
+
+            <S.Title>
+                <h3>TAREFAS</h3>
+            </S.Title>
+            <S.Content>
+            {/* TaskCard */}
+            </S.Content>
+            {/* Header */}
+        </S.Container>
+    )
+}
+
+export default Home;
